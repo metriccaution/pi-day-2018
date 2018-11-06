a2231e5a9f573933a8aa88ae82675b9efb57a21c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-06 12:25:36 +0000

    Deploy metriccaution/pi-day-2018 to github.com/metriccaution/pi-day-2018.git:gh-pages

---
+++ precache-manifest.f133d9d052888043e0074f8107a29563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21e996ac56c4eb744908",
+    "url": "/pi-day-2018/static/js/runtime~main.21e996ac.js"
+  },
+  {
+    "revision": "edb41feabb7db9f3f04f",
+    "url": "/pi-day-2018/static/js/main.edb41fea.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/pi-day-2018/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "edb41feabb7db9f3f04f",
+    "url": "/pi-day-2018/static/css/main.c81171d6.chunk.css"
+  },
+  {
+    "revision": "f8d752db749507ae66d6f357d04b3581",
+    "url": "/pi-day-2018/index.html"
+  }
+];
